c799e32294f96002b77c0824f497459a3f7d60e5
James <jamezcrook@gmail.com>
2024-10-22 01:10:58 +0100

    implamenting pop over drawers

--- src/lib/components/navigation/drawer/Drawer.guide.svelte
+++ src/lib/components/navigation/drawer/Drawer.guide.svelte
@@ -1,12 +1,87 @@
-
 <script lang="ts">
-    import  Drawer from './Drawer.svelte';
+	import Button from '$lib/components/forms/buttons/button/Button.svelte';
+	import Drawer from './Drawer.svelte';
 
-    const props = {
-        label: '!!!',
-    };
+	const group = [
+		{ label: 'Home', href: '/' },
+		{ label: 'Guide', href: '/guide' },
+		{ label: 'Navigation', href: '/guide/navigation' },
+		{ label: 'Drawer', href: '/guide/navigation/drawer' }
+	];
 
+	const props = [
+		[
+			{
+				label: 'Pop Over Right',
+				group,
+				variant: 'over',
+				direction: 'right',
+				id: 'pop-over-right'
+			},
+			{
+				label: 'Pop Over Left',
+				group,
+				variant: 'over',
+				direction: 'left',
+				id: 'pop-over-left'
+			},
+			{
+				label: 'Pop Over Top',
+				group,
+				variant: 'over',
+				direction: 'top',
+				id: 'pop-over-top'
+			},
+			{
+				label: 'Pop Over Bottom',
+				group,
+				variant: 'over',
+				direction: 'bottom',
+				id: 'pop-over-bottom'
+			}
+		]
+		// [
+		// 	{
+		// 		label: 'Push Open Right',
+		// 		group,
+		// 		variant: 'push',
+		// 		direction: 'right',
+		// 		id: 'push-open-right'
+		// 	},
+		// 	{
+		// 		label: 'Push Open Left',
+		// 		group,
+		// 		variant: 'push',
+		// 		direction: 'left',
+		// 		id: 'push-open-left'
+		// 	},
+		// 	{
+		// 		label: 'Push Open Top',
+		// 		group,
+		// 		variant: 'push',
+		// 		direction: 'top',
+		// 		id: 'push-open-top'
+		// 	},
+		// 	{
+		// 		label: 'Push Open Bottom',
+		// 		group,
+		// 		variant: 'push',
+		// 		direction: 'bottom',
+		// 		id: 'push-open-bottom'
+		// 	}
+		// ]
+	];
+
+	let activeId = '';
 </script>
 
-<Drawer {...props} />
-
+{#each props as groupType}
+	<div class="demo-variant">
+		{#each groupType as prop, index}
+			<Button on:click={() => (activeId === prop.id ? (activeId = '') : (activeId = prop.id))}
+				>{prop.label}</Button
+			>
+			<Drawer {...prop} {activeId} />
+		{/each}
+	</div>
+{/each}
--- src/lib/components/navigation/drawer/Drawer.svelte
+++ src/lib/components/navigation/drawer/Drawer.svelte
@@ -1,10 +1,37 @@
-
 <script lang="ts">
-    import './Drawer.scss';
-    import handleDrawer from './Drawer.js';
-    export let label: string = '';
+	import './Drawer.scss';
+	import Button from '$lib/components/forms/buttons/button/Button.svelte';
+	import ButtonGroup from '$lib/components/forms/buttons/buttonGroup/ButtonGroup.svelte';
+	export let label: string = '';
+	export let group: any[] = [];
+	export let direction: string = 'left';
+	export let activeId = '';
+	export let active = false;
+	export let id = '';
+	export let variant: string = 'over';
 
-    handleDrawer();
+	$: isOpen = active || activeId === id;
+
+	$: openClass = isOpen ? 'open' : 'closed';
+
+	$: buttonVariant = direction === 'left' || direction === 'right' ? 'column' : 'row';
+
+	const handleClose = () => {
+		active = false;
+		activeId = '';
+	};
 </script>
 
-<div class="Drawer-container">Drawer Placeholder{label}</div>
+<div {id} class="drawer {direction} {openClass} {variant}">
+	<div class="drawer-headder">
+		{#if label}
+			<h2 class="drawer-label">{label}</h2>
+		{/if}
+		<Button label="Close" on:click={() => handleClose()} />
+	</div>
+	{#if group.length > 0}
+		<ButtonGroup {group} variant={buttonVariant} />
+	{:else}
+		<slot />
+	{/if}
+</div>
