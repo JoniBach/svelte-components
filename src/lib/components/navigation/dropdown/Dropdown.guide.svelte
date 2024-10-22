7b238010fbb3d8f5bf7aea8d2642f4c6edb19353
James <jamezcrook@gmail.com>
2024-10-22 20:57:08 +0100

    implamenting dropdown

--- src/lib/components/navigation/dropdown/Dropdown.guide.svelte
+++ src/lib/components/navigation/dropdown/Dropdown.guide.svelte
@@ -1,12 +1,89 @@
-
 <script lang="ts">
-    import  Dropdown from './Dropdown.svelte';
+	import Button from '$lib/components/forms/buttons/button/Button.svelte';
+	import Dropdown from './Dropdown.svelte';
 
-    const props = {
-        label: '!!!',
-    };
+	const group = [
+		{ label: 'Home', href: '/' },
+		{ label: 'Guide', href: '/guide' },
+		{ label: 'Navigation', href: '/guide/navigation' },
+		{ label: 'Dropdown', href: '/guide/navigation/dropdown' }
+	];
 
+	const props = [
+		[
+			{
+				label: 'Drawer Column Bottom',
+				group,
+				variant: 'column',
+				direction: 'bottom',
+				id: 'pop-over-bottom'
+			},
+			{
+				label: 'Drawer Column Right',
+				group,
+				variant: 'column',
+				direction: 'right',
+				id: 'pop-over-right'
+			},
+			{
+				label: 'Drawer Column Left',
+				group,
+				variant: 'column',
+				direction: 'left',
+				id: 'pop-over-left'
+			},
+			{
+				label: 'Drawer Column Top',
+				group,
+				variant: 'column',
+				direction: 'top',
+				id: 'pop-over-top'
+			}
+		],
+		[
+			{
+				label: 'Drawer row Bottom',
+				group,
+				variant: 'row',
+				direction: 'bottom',
+				id: 'pop-row-bottom'
+			},
+			{
+				label: 'Drawer row Right',
+				group,
+				variant: 'row',
+				direction: 'right',
+				id: 'pop-row-right'
+			},
+			{
+				label: 'Drawer row Left',
+				group,
+				variant: 'row',
+				direction: 'left',
+				id: 'pop-row-left'
+			},
+			{
+				label: 'Drawer row Top',
+				group,
+				variant: 'row',
+				direction: 'top',
+				id: 'pop-row-top'
+			}
+		]
+	];
+
+	let activeId = '';
 </script>
 
-<Dropdown {...props} />
+{#each props as groupType}
+	<div class="demo-variant">
+		{#each groupType as prop, index}
+			<Dropdown {...prop} {activeId} />
+		{/each}
+	</div>
+{/each}
 
+<div class="demo-variant">
+	<Dropdown {...props[0][0]} />
+	<Dropdown {...props[1][1]} />
+</div>
--- src/lib/components/navigation/dropdown/Dropdown.svelte
+++ src/lib/components/navigation/dropdown/Dropdown.svelte
@@ -1,10 +1,34 @@
-
 <script lang="ts">
-    import './Dropdown.scss';
-    import handleDropdown from './Dropdown.js';
-    export let label: string = '';
+	import './Dropdown.scss';
+	import Button from '$lib/components/forms/buttons/button/Button.svelte';
+	import ButtonGroup from '$lib/components/forms/buttons/buttonGroup/ButtonGroup.svelte';
+	export let label: string = '';
+	export let group: any[] = [];
+	export let direction: string = 'left';
+	export let activeId = '';
+	export let active = false;
+	export let id = '';
+	export let variant: string = 'column';
 
-    handleDropdown();
+	$: isOpen = active || activeId === id;
+
+	$: openClass = isOpen ? 'open' : 'closed';
+
+	const handleClose = () => {
+		active = false;
+		activeId = '';
+	};
 </script>
 
-<div class="Dropdown-container">Dropdown Placeholder{label}</div>
+<div class="dropdown-wrapper">
+	<Button on:click={() => (activeId === id ? (activeId = '') : (activeId = id))}>{label}</Button>
+	<div {id} class="dropdown {direction} {openClass} {variant}">
+		{#if group.length > 0}
+			<ButtonGroup {group} {variant} />
+		{:else if 'label'}
+			<Button label="Close" on:click={() => handleClose()} />
+		{:else}
+			<slot />
+		{/if}
+	</div>
+</div>
