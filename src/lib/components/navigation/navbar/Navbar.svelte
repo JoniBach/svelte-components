1e4d605e177aa080873e5ddeeb70f972a0961ad4
James <jamezcrook@gmail.com>
2024-10-23 00:18:36 +0100

    adding nav bar

--- src/lib/components/navigation/navbar/Navbar.svelte
+++ src/lib/components/navigation/navbar/Navbar.svelte
@@ -1,10 +1,25 @@
-
 <script lang="ts">
-    import './Navbar.scss';
-    import handleNavbar from './Navbar.js';
-    export let label: string = '';
-
-    handleNavbar();
+	import Dropdown from './../dropdown/Dropdown.svelte';
+	import './Navbar.scss';
+	export let label: string = '';
+	export let href: string = '/';
+	export let group: array = [];
+	export let gap: string = 'small';
 </script>
 
-<div class="Navbar-container">Navbar Placeholder{label}</div>
+<nav class="navbar">
+	<a class="navbar-title" {href}>
+		{label}
+	</a>
+
+	<div class="navbar-menu gap-{gap}">
+		{#each group as item}
+			<!-- <a class="navbar-link" href={item.href} target={item.target} rel={item.rel}>
+				{item.label}
+			</a> -->
+			<Dropdown group={item.group} label={item.label} anchor="right" />
+		{/each}
+
+		<slot />
+	</div>
+</nav>
