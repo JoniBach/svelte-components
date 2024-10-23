475a1cb6c871b921048b476694fe71bb94b316fa
James <jamezcrook@gmail.com>
2024-10-24 00:19:44 +0100

    implamenting alert

--- src/lib/components/display/data/alert/Alert.guide.svelte
+++ src/lib/components/display/data/alert/Alert.guide.svelte
@@ -1,12 +1,106 @@
-
 <script lang="ts">
-    import  Alert from './Alert.svelte';
+	import Button from '$lib/components/forms/buttons/button/Button.svelte';
+	import ButtonGroup from '$lib/components/forms/buttons/buttonGroup/ButtonGroup.svelte';
+	import Alert from './Alert.svelte';
 
-    const props = {
-        label: '!!!',
-    };
+	let group = [];
 
+	$: props = {
+		group
+	};
+
+	function handleClick(e) {
+		const id = e.detail.buttonId;
+		const time = new Date();
+
+		// Use a reactive assignment to trigger reactivity in Svelte.
+		switch (id) {
+			case 'error':
+				group = [
+					...group,
+					{
+						label: 'Save error',
+						description: "Oops! We couldn't save your document. Please try again later.",
+						code: 404,
+						variant: 'error',
+						time
+					}
+				];
+				break;
+			case 'warning':
+				group = [
+					...group,
+					{
+						label: 'Space warning',
+						description: 'We saved your changes! But you are running out of storage!',
+						code: null,
+						variant: 'warning',
+						time
+					}
+				];
+				break;
+			case 'success':
+				group = [
+					...group,
+					{
+						label: 'Saved successfully',
+						description: 'Nice! Your changes have been successfully saved!',
+						code: 200,
+						variant: 'success',
+						time
+					}
+				];
+				break;
+			case 'info':
+				group = [
+					...group,
+					{
+						label: 'Storage info',
+						description: 'You are currently using 2.5GB of your storage.',
+						code: 200,
+						variant: 'info',
+						time
+					}
+				];
+				break;
+			default:
+				break;
+		}
+	}
+
+	const buttons = [
+		{
+			id: 'error',
+			label: 'Error',
+			color: 'danger',
+			variant: 'outlined'
+		},
+		{
+			id: 'warning',
+			label: 'Warning',
+			color: 'warning',
+			variant: 'outlined'
+		},
+		{
+			id: 'success',
+			label: 'Success',
+			color: 'success',
+			variant: 'outlined'
+		},
+		{
+			id: 'info',
+			label: 'Info',
+			color: 'info',
+			variant: 'outlined'
+		}
+	];
+
+	function handleAlertClick(e) {
+		alert(e.detail.description);
+	}
 </script>
 
-<Alert {...props} />
-
+<div class="demo-variant">
+	<Alert {...props} on:click={handleAlertClick} />
+	<ButtonGroup id="alert-triggers" label="Open Alert" group={buttons} on:click={handleClick} />
+</div>
--- src/lib/components/display/data/alert/Alert.svelte
+++ src/lib/components/display/data/alert/Alert.svelte
@@ -1,10 +1,56 @@
-
 <script lang="ts">
-    import './Alert.scss';
-    import handleAlert from './Alert.js';
-    export let label: string = '';
+	import { createEventDispatcher } from 'svelte';
+	import './Alert.scss';
+	import Dropdown from '$lib/components/navigation/dropdown/Dropdown.svelte';
+	export let group: any[] = [];
 
-    handleAlert();
+	$: latest = group.length > 0 ? group[group.length - 1] : null;
+
+	$: activeVariants = Array.from(new Set(group.map((item) => item.variant)));
+	$: groupedVariants = activeVariants.map((variant) => {
+		return {
+			variant: variant,
+			group: group.filter((item) => item.variant === variant),
+			latest: group.filter((item) => item.variant === variant)[0],
+			count: group.filter((item) => item.variant === variant).length
+		};
+	});
+
+	function handleClear() {
+		group = [];
+	}
+	const dispatch = createEventDispatcher();
 </script>
 
-<div class="Alert-container">Alert Placeholder{label}</div>
+{#if latest}
+	<div on:click={() => dispatch('click', latest)} class="alert variant-{latest.variant || 'info'}">
+		<p class="alert-text">{latest.label}</p>
+		<div class="alert-group">
+			{#each groupedVariants as variantGroup}
+				<Dropdown
+					group={variantGroup.group}
+					label="{variantGroup.count} {variantGroup.variant}"
+					anchor="right"
+					size="tiny"
+				/>
+			{/each}
+			<button class="alert-text" on:click={handleClear}>
+				<svg
+					xmlns="http://www.w3.org/2000/svg"
+					viewBox="0 0 24 24"
+					width="1em"
+					height="1em"
+					fill="none"
+					stroke="currentColor"
+					stroke-width="2"
+					stroke-linecap="round"
+					stroke-linejoin="round"
+					class="icon-cross"
+				>
+					<line x1="18" y1="6" x2="6" y2="18"></line>
+					<line x1="6" y1="6" x2="18" y2="18"></line>
+				</svg>
+			</button>
+		</div>
+	</div>
+{/if}
