5904fb5c2e6f7a529d5c9e1a05ee8130dfa05839
James <jamezcrook@gmail.com>
2024-10-25 00:04:06 +0100

    relocate validation

---
+++ src/lib/components/forms/formValidation/FormValidation.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+	import './FormValidation.scss';
+	import handleFormvalidation from './formValidation.js';
+	export let label: string = '';
+
+	handleFormvalidation();
+</script>
+
+<div class="FormValidation-container">Formvalidation Placeholder{label}</div>
