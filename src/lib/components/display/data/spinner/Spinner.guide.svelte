fffa0ade67c1a8ff6692e84937b66bb095650323
James <jamezcrook@gmail.com>
2024-10-24 23:46:54 +0100

    adding spinner

--- src/lib/components/display/data/spinner/Spinner.guide.svelte
+++ src/lib/components/display/data/spinner/Spinner.guide.svelte
@@ -1,12 +1,60 @@
-
 <script lang="ts">
-    import  Spinner from './Spinner.svelte';
+	import Spinner from './Spinner.svelte';
 
-    const props = {
-        label: '!!!',
-    };
-
+	const props = [
+		[
+			{
+				size: 'xl',
+				color: 'accent',
+				borderWidth: 'tiny'
+			},
+			{
+				size: 'large',
+				color: 'primary',
+				borderWidth: 'tiny'
+			},
+			{
+				size: 'medium',
+				color: 'secondary',
+				borderWidth: 'tiny'
+			},
+			{
+				size: 'small',
+				color: 'tertiary',
+				borderWidth: 'tiny'
+			}
+		],
+		[
+			{
+				size: 'xl',
+				color: 'accent',
+				borderWidth: 'small'
+			},
+			{
+				size: 'large',
+				color: 'primary',
+				borderWidth: 'small'
+			},
+			{
+				size: 'medium',
+				color: 'secondary',
+				borderWidth: 'small'
+			}
+		],
+		[
+			{
+				size: 'xl',
+				color: 'accent',
+				borderWidth: 'medium'
+			}
+		]
+	];
 </script>
 
-<Spinner {...props} />
-
+{#each props as prop}
+	<div class="demo-variant flex">
+		{#each prop as { size, color, borderWidth }}
+			<Spinner {size} {color} {borderWidth} />
+		{/each}
+	</div>
+{/each}
--- src/lib/components/display/data/spinner/Spinner.svelte
+++ src/lib/components/display/data/spinner/Spinner.svelte
@@ -1,10 +1,11 @@
-
-<script lang="ts">
-    import './Spinner.scss';
-    import handleSpinner from './Spinner.js';
-    export let label: string = '';
-
-    handleSpinner();
+<script>
+	export let size = 'xl'; // default size category
+	export let color = 'accent'; // default to a plain string, can be overridden by parent component
+	export let borderWidth = 'tiny'; // default border width, plain string
+	import './spinner.scss';
 </script>
 
-<div class="Spinner-container">Spinner Placeholder{label}</div>
+<div
+	class="spinner {size} border-{borderWidth}"
+	style="--color: var(--color-{color}; --borderWidth: var(--spacing-{borderWidth});"
+></div>
