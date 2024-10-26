fb704ccb5b6e5053dc0d2b3a33a59d330dee10b6
James <jamezcrook@gmail.com>
2024-10-26 15:15:22 +0100

    denesting display components

---
+++ src/lib/components/display/avatar/Avatar.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+	import './Avatar.scss';
+	import handleAvatar from './avatar.js';
+	export let label: string = '';
+
+	handleAvatar();
+</script>
+
+<div class="Avatar-container">Avatar Placeholder{label}</div>
