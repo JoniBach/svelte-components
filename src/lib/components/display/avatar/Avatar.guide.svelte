3b1ed21efa4e19e84b51eef4837c5c5f4ebc0073
James <jamezcrook@gmail.com>
2024-10-26 15:47:04 +0100

    implament avatar

--- src/lib/components/display/avatar/Avatar.guide.svelte
+++ src/lib/components/display/avatar/Avatar.guide.svelte
@@ -1,12 +1,30 @@
-
 <script lang="ts">
-    import  Avatar from './Avatar.svelte';
+	import Avatar from './Avatar.svelte';
 
-    const props = {
-        label: '!!!',
-    };
-
+	const props = [
+		{
+			src: 'https://i.pravatar.cc/300',
+			variant: 'circle',
+			space: 'tiny'
+		},
+		{
+			label: 'Alex Jones',
+			src: 'https://i.pravatar.cc/300',
+			variant: 'rounded',
+			space: 'small'
+		},
+		{
+			label: 'Alex Jones',
+			subtext: 'Admin',
+			src: 'https://i.pravatar.cc/300',
+			variant: 'square',
+			space: 'medium'
+		}
+	];
 </script>
 
-<Avatar {...props} />
-
+<div class="demo-variant">
+	{#each props as prop}
+		<Avatar {...prop} />
+	{/each}
+</div>
--- src/lib/components/display/avatar/Avatar.svelte
+++ src/lib/components/display/avatar/Avatar.svelte
@@ -1,9 +1,33 @@
 <script lang="ts">
 	import './Avatar.scss';
 	import handleAvatar from './avatar.js';
+
 	export let label: string = '';
+	export let subtext: string = '';
+	export let src: string = '';
+	export let variant: string = '';
+	export let space: string = '';
 
 	handleAvatar();
 </script>
 
-<div class="Avatar-container">Avatar Placeholder{label}</div>
+<div class="avatar-wrapper space-{space} {variant} " on:click>
+	<div class="avatar {variant}">
+		{#if src}
+			<img {src} alt={label} class="avatar__image" />
+		{:else}
+			<span class="avatar__placeholder">{label ? label[0].toUpperCase() : '?'}</span>
+		{/if}
+	</div>
+
+	{#if label}
+		<div class="avatar__text-panel">
+			{#if label}
+				<span class="avatar__label">{label}</span>
+			{/if}
+			{#if subtext}
+				<span class="avatar__subtext">{subtext}</span>
+			{/if}
+		</div>
+	{/if}
+</div>
