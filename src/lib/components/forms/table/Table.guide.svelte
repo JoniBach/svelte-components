56d86f83d692ce2492069859f5c2a454b1b30d38
James <jamezcrook@gmail.com>
2024-10-31 12:00:20 +0000

    create blank table

---
+++ src/lib/components/forms/table/Table.guide.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+	import Table from './Table.svelte';
+
+	let value = {
+		name: 'test'
+	};
+
+	const props = [
+		{
+			label: 'table',
+			group: []
+		}
+	];
+</script>
+
+{#each props as props}
+	<div class="demo-variant">
+		<Table {...props} on:input />
+	</div>
+{/each}
---
+++ src/lib/components/forms/table/Table.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+	import { createEventDispatcher } from 'svelte';
+
+	export let group = [];
+	export let value = {};
+
+	const dispatch = createEventDispatcher();
+</script>
