359f518a6adc9af92f446f7fcdb08f7eeeccc236
James <jamezcrook@gmail.com>
2024-11-03 02:09:18 +0000

    tabs

--- src/lib/components/layout/content/tabs/Tabs.guide.svelte
+++ src/lib/components/layout/content/tabs/Tabs.guide.svelte
@@ -1,9 +1,24 @@
-<script lang="ts">
-	import Tabs from './tabs/Tabs.svelte';
-
-	const props = {
-		label: '!!!'
-	};
+<script>
+	import Tabs from './Tabs.svelte';
+	let active = '';
+	const group = [
+		{ label: 'Tab 1', id: 'tab-1' },
+		{ label: 'Tab 2', id: 'tab-2' },
+		{ label: 'Tab 3', id: 'tab-3' }
+	];
+	function handleClick(event) {
+		active = event.detail.id;
+	}
 </script>
 
-<Tabs {...props} />
+<Tabs {group} {active} on:click={handleClick}>
+	{#if active === 'tab-1'}
+		<div>Tab 1 Content</div>
+	{/if}
+	{#if active === 'tab-2'}
+		<div>Tab 2 Content</div>
+	{/if}
+	{#if active === 'tab-3'}
+		<div>Tab 3 Content</div>
+	{/if}
+</Tabs>
--- src/lib/components/layout/content/tabs/Tabs.svelte
+++ src/lib/components/layout/content/tabs/Tabs.svelte
@@ -1,10 +1,32 @@
-
 <script lang="ts">
-    import './Tabs.scss';
-    import handleTabs from './Tabs.js';
-    export let label: string = '';
+	import { createEventDispatcher, onMount } from 'svelte';
 
-    handleTabs();
+	export let group = [];
+	export let active = '';
+	import './tabs.scss';
+
+	const dispatch = createEventDispatcher();
+
+	const selectTab = (id) => {
+		dispatch('click', { id });
+	};
+	onMount(() => {
+		selectTab(group[0].id);
+	});
 </script>
 
-<div class="Tabs-container">Tabs Placeholder{label}</div>
+<div class="tabs">
+	<div class="tab-headers">
+		{#each group as tab}
+			<div
+				class="tab-header {tab.id === active ? 'active' : ''}"
+				on:click={() => selectTab(tab.id)}
+			>
+				{tab.label}
+			</div>
+		{/each}
+	</div>
+</div>
+<div class="tab-content">
+	<slot />
+</div>
