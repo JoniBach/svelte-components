9e1831501dcb17137be9ffe86eca9c60cd3969d6
James <jamezcrook@gmail.com>
2024-11-03 00:51:43 +0000

    implamenting confirm dialogue

--- src/lib/components/feedback/confirmDialog/ConfirmDialog.guide.svelte
+++ src/lib/components/feedback/confirmDialog/ConfirmDialog.guide.svelte
@@ -1,12 +1,31 @@
-
 <script lang="ts">
-    import  Confirmdialog from './ConfirmDialog.svelte';
+	import Button from '$lib/components/forms/buttons/button/Button.svelte';
+	import Confirmdialog from './ConfirmDialog.svelte';
 
-    const props = {
-        label: '!!!',
-    };
+	let active = false;
+	const group = [
+		{ id: 'cancel', label: 'Cancel', variant: 'outlined', color: 'danger' },
+		{ id: 'confirm', label: 'Confirm', variant: 'solid', color: 'success' }
+	];
 
+	$: props = {
+		label: 'Save document',
+		id: 'save-document',
+		group,
+		active
+	};
+
+	const handleConfirm = (e) => {
+		alert('confirmed with the following event details:' + JSON.stringify(e.detail));
+		active = false;
+	};
+
+	const handleCancel = (e) => {
+		alert('cancelled with the following event details:' + JSON.stringify(e.detail));
+		active = false;
+	};
 </script>
 
-<Confirmdialog {...props} />
+<Confirmdialog {...props} on:confirm={(e) => handleConfirm(e)} on:cancel={(e) => handleCancel(e)} />
 
+<Button on:click={() => (active = true)}>Open Dialog</Button>
--- src/lib/components/feedback/confirmDialog/ConfirmDialog.svelte
+++ src/lib/components/feedback/confirmDialog/ConfirmDialog.svelte
@@ -1,10 +1,28 @@
-
 <script lang="ts">
-    import './ConfirmDialog.scss';
-    import handleConfirmdialog from './ConfirmDialog.js';
-    export let label: string = '';
+	import { createEventDispatcher, onMount } from 'svelte';
+	import './ConfirmDialog.scss';
+	import ButtonGroup from '$lib/components/forms/buttons/buttonGroup/ButtonGroup.svelte';
 
-    handleConfirmdialog();
+	export let label: string = 'Confirmation';
+	export let description: string = 'Are you sure?';
+	export let id: string = 'confirm-dialog';
+	export let active: boolean = false;
+	export let position: string = 'top';
+	export let group: string[] = [
+		{ id: 'cancel', label: 'Cancel', variant: 'outlined', color: 'danger' },
+		{ id: 'confirm', label: 'Confirm', variant: 'solid', color: 'success' }
+	];
+	const dispatch = createEventDispatcher();
+
+	const handleClick = (event: CustomEvent) => dispatch(event.detail.buttonId, event.detail);
 </script>
 
-<div class="ConfirmDialog-container">Confirmdialog Placeholder{label}</div>
+<div class="confirm-dialog dialog-{position} {active ? 'active' : ''}">
+	<div class="dialog-content">
+		<p class="dialog-title">{label}</p>
+		<p class="dialog-description">{description}</p>
+		<div class="dialog-actions">
+			<ButtonGroup {id} {group} on:click={(event) => handleClick(event)} />
+		</div>
+	</div>
+</div>
