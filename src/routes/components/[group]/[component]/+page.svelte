f30dcf206a38fa82e9fcd02d7ea8ce5f99df0e14
James <jamezcrook@gmail.com>
2024-11-05 16:30:35 +0000

    refining code

--- src/routes/components/[group]/[component]/+page.svelte
+++ src/routes/components/[group]/[component]/+page.svelte
@@ -1,46 +1,45 @@
-<script>
+<script lang="ts">
+	import { onMount } from 'svelte';
+	import { goto } from '$app/navigation';
+	import { page } from '$app/stores';
 	import Navbar from '$lib/components/navigation/navbar/Navbar.svelte';
 	import Button from '$lib/components/forms/button/Button.svelte';
-	import { changeTheme } from '$lib/index';
-	import { onMount } from 'svelte';
 	import Menu from '$lib/components/navigation/menu/Menu.svelte';
-	import { page } from '$app/stores';
 	import Table from '$lib/components/forms/table/Table.svelte';
-	import { goto } from '$app/navigation';
+	import { changeTheme } from '$lib/index';
+
 	let components = { groups: [] };
 	let DynamicComponent;
+	let componentName,
+		groupName,
+		component,
+		props = {};
+	let label, group, menuGroup;
 
-	onMount(() => {
-		fetch('/components.json')
-			.then((res) => res.json())
-			.then((data) => {
-				components = data;
-			})
-			.catch((error) => {
-				console.error('Error fetching components:', error);
-			});
+	onMount(async () => {
+		try {
+			const response = await fetch('/components.json');
+			components = await response.json();
+		} catch (error) {
+			console.error('Error fetching components:', error);
+		}
 	});
 
 	$: componentName = $page.params.component;
 	$: groupName = $page.params.group;
 
-	// Dynamically import the component whenever the URL parameters change
 	$: if (componentName && groupName) {
-		let componentFolderName = componentName.charAt(0).toLowerCase() + componentName.slice(1);
-		let componentPath = `../../../../lib/components/${groupName}/${componentFolderName}/${componentName}.svelte`;
+		const componentFolderName = componentName[0].toLowerCase() + componentName.slice(1);
+		const componentPath = `../../../../lib/components/${groupName}/${componentFolderName}/${componentName}.svelte`;
 
-		import(componentPath)
-			.then((mod) => {
-				DynamicComponent = mod.default;
+		import(/* @vite-ignore */ componentPath)
+			.then(({ default: loadedComponent }) => {
+				DynamicComponent = loadedComponent;
 			})
 			.catch((err) => {
 				console.error('Error loading component:', componentPath, err);
 				DynamicComponent = null;
 			});
-	}
-
-	function handleClick(e) {
-		goto(`/components/${e.detail.buttonGroupId}/${e.detail.buttonId}`);
 	}
 
 	$: component = components.groups
@@ -49,83 +48,69 @@
 
 	$: props = component ? mapPropsToDefaults(component.props) : {};
 
-	function mapPropsToDefaults(propsArray) {
-		if (!propsArray) return {};
+	function mapPropsToDefaults(propsArray = []) {
 		return propsArray.reduce((acc, { prop, example: defaultValue }) => {
 			acc[prop] = defaultValue;
 			return acc;
 		}, {});
 	}
 
-	$: label = components?.title;
-	$: group = components?.nav;
-	$: menuGroup = components?.groups;
+	function handleClick({ detail: { buttonGroupId, buttonId } }) {
+		goto(`/components/${buttonGroupId}/${buttonId}`);
+	}
+
+	$: {
+		const { label: newLabel, nav, groups } = components;
+		label = newLabel;
+		group = nav;
+		menuGroup = groups;
+	}
 </script>
 
-{#if menuGroup && menuGroup.length && component}
-	<Navbar label={label + ' - ' + component.name} {group}>
-		<Button label="Theme" on:click={() => changeTheme()} />
+{#if menuGroup?.length && component}
+	<Navbar label={`${label} - ${component.name}`} {group}>
+		<Button label="Theme" on:click={changeTheme} />
 	</Navbar>
-	<div class="page">
-		{#each menuGroup as menu}
+	<div class="component-page">
+		{#each menuGroup as { id, label, components }}
 			<Menu
-				id={menu.id}
-				label={menu.label}
-				on:click={(e) => handleClick(e)}
-				group={menu.components.map((component) => ({
-					label: component.name,
-					id: component.name,
-					size: 'xs'
-				}))}
+				{id}
+				{label}
+				group={components.map(({ name }) => ({ label: name, id: name, size: 'xs' }))}
 				align="left"
+				on:click={handleClick}
 			/>
 		{/each}
-		{#if DynamicComponent && component}
-			<div class="page-body">
+		<div class="component-body">
+			{#if DynamicComponent}
 				<svelte:component this={DynamicComponent} {...props} />
 				<Table
 					form
 					label="{component.name} props"
 					bind:value={component.props}
 					group={[
-						{
-							variant: 'text',
-							name: 'prop',
-							label: 'prop',
-							disabled: true
-						},
-						{
-							variant: 'text',
-							name: 'type',
-							label: 'type',
-							disabled: true
-						},
-						{
-							variant: 'text',
-							name: 'default',
-							label: 'default',
-							disabled: true
-						},
-						{
-							variant: 'json',
-							name: 'example',
-							label: 'example'
-						}
+						{ variant: 'text', name: 'prop', label: 'Prop', disabled: true },
+						{ variant: 'text', name: 'type', label: 'Type', disabled: true },
+						{ variant: 'text', name: 'default', label: 'Default', disabled: true },
+						{ variant: 'json', name: 'example', label: 'Example' }
 					]}
 				/>
-			</div>
-		{:else}
-			<p>Not found</p>
-		{/if}
+			{:else}
+				<p>Component not found.</p>
+			{/if}
+		</div>
 	</div>
 {/if}
 
 <style>
-	.page {
+	.component-page {
 		display: flex;
 		height: 100vh;
 	}
-	.page-body {
+
+	.component-body {
 		margin: var(--spacing-large);
+		flex-grow: 1;
+		overflow: auto;
 	}
 </style>
