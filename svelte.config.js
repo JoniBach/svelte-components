bd16d366c54ba004148fd9692a19afe07eeb555b
James <jamezcrook@gmail.com>
2024-11-07 00:05:37 +0000

    svelte preprocess

--- svelte.config.js
+++ svelte.config.js
@@ -1,19 +1,17 @@
 import adapter from '@sveltejs/adapter-auto';
 import { sveltePreprocess } from 'svelte-preprocess';
-import sass from 'sass'; // Using ES module import style
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
-	// for more information about preprocessors
 	preprocess: sveltePreprocess({
-		scss: true
+		scss: {
+			// Remove `sass` import and ensure this section is configured correctly
+		},
+		// Optionally, enable sourceMap if you need debugging information for styles
+		sourceMap: true
 	}),
 
 	kit: {
-		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
-		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
-		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
 		adapter: adapter()
 	}
 };
