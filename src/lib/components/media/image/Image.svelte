dc86f78f652a9ce3f009a4b978ab17fe1c030437
James <jamezcrook@gmail.com>
2024-11-07 00:16:01 +0000

    fixing impoirts

--- src/lib/components/media/image/Image.svelte
+++ src/lib/components/media/image/Image.svelte
@@ -1,9 +1,6 @@
 <script lang="ts">
 	import './image.scss';
-	import handleImage from './Image.js';
 	export let label: string = '';
-
-	handleImage();
 </script>
 
 <div class="Image-container">Image Placeholder{label}</div>
