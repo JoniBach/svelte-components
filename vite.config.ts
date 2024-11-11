67cdf2f088283a7a777754cebc3c9b67d463ea1d
James <jamezcrook@gmail.com>
2024-11-11 00:58:26 +0000

    adding dynamic imports

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,12 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vitest/config';
+import dynamicImport from 'vite-plugin-dynamic-import';
 
 export default defineConfig({
-	plugins: [sveltekit()],
+	plugins: [
+		sveltekit(),
+		dynamicImport() // Add the plugin here
+	],
 	test: {
 		include: ['src/**/*.{test,spec}.{js,ts}']
 	}
